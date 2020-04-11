003ff0f87e95c00261a463f672c2ccfc1a33ae27
Jakub Gniadek <kuballo.g@gmail.com>
2020-04-11 12:36:37 +0200

    Updates

---
+++ precache-manifest.a52127ce7340fd6b68876ffd2d3d0de3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "188ed4b966c31eeaf39e83a81811c36c",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "cf724c3aca35eca4739d",
+    "url": "/Calculator/static/css/main.24c5ffd5.chunk.css"
+  },
+  {
+    "revision": "eb7f49831f863618dd21",
+    "url": "/Calculator/static/js/2.8cc044c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.8cc044c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf724c3aca35eca4739d",
+    "url": "/Calculator/static/js/main.52916bad.chunk.js"
+  },
+  {
+    "revision": "ecedcecfa5e69224ee2c",
+    "url": "/Calculator/static/js/runtime-main.2da71ecd.js"
+  }
+]);
